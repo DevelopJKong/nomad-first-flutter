5331e2fae4013492a4cdfef462ca774459d1d554
Park JeongBin <ybaram40@gmail.com>
2023-06-06 22:18:57 +0900

    #3.0 Header 완료

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        title: const Text('Hello World'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    ));
+            backgroundColor: Colors.black,
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('Hey, Selena',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 38,
+                                    fontWeight: FontWeight.w800)),
+                            Text('Welcome back',
+                                style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: 22)),
+                          ])
+                    ],
+                  )
+                ],
+              ),
+            )));
   }
 }
